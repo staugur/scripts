1ab2ff0455360f48cc8b6a5ed1c85a537cbc6590
陶成伟 <staugur@vip.qq.com>
2015-11-03 14:13:43 +0800

    keepalived configure

--- services/Nginx_HA.sh
+++ services/Nginx_HA.sh
@@ -3,69 +3,39 @@
 if [ $(id -u) != "0" ]; then
   echo "Error:请确保以root用户执行此脚本！" ; exit 1
 fi
-if [ $# -ne 2 ];then
-  echo "Error:需要两个类型参数,$0 master/backup virtual_ip";
-  exit 1
-fi
-ha_version="1.2.17"
-ha_conf="/etc/keepalived"
-app_dir="/data/app"
-soft_dir="/data/software"
-ha_type=$1
-vip=$2
-dev="eth0"
-#check ip format
-_check_ip=`echo $vip | egrep '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'`
-[ ! $_check_ip ] && echo "IP地址格式不正确" && exit 3
-
-[ -d ${soft_dir} ] || mkdir -p ${soft_dir}
-[ -d ${app_dir} ] || mkdir -p ${app_dir}
 
 function download() {
   cd ${soft_dir};
-  curl https://saintic.top/nginx.txt > nginx.sh
   wget -c http://www.keepalived.org/software/keepalived-${ha_version}.tar.gz
-}
-
-function nginx() {
-  if [ $(which nginx|wc -l) -ne 0 ];then
-    echo "Nginx has been installed"
-  elif [ $(ps aux|grep -v grep|grep nginx|wc -l) -ne 0 ];then
-    echo "Nginx has been installed!"
-  else:
-    echo "未安装Nginx服务，可能的原因是搜索不到nginx命令或尚未安装Nginx服务!"
-    echo "将执行Nginx安装脚本"
-    cd ${soft_dir}; sh nginx.sh
-  fi
 }
 
 function master() {
 cat > keepalived.conf <<EOF
 ! Configuration File for keepalived
 global_defs {
-   router_id nginx
+   router_id nginx_master
 }
 vrrp_script Monitor_Nginx {
    script "/usr/local/nginx_pid.sh"
    interval 2
-   weight 2
+   weight -10
 }
 vrrp_instance nginx {
     state MASTER
     interface ${dev}
-    virtual_router_id 51
+    virtual_router_id 99
     priority 101
     advert_int 1
     authentication {
         auth_type PASS
-        auth_pass saintic
+        auth_pass 123456
     }
     virtual_ipaddress {
         $vip
     }
-}
-track_script {
-    Monitor_Nginx
+    track_script {
+        Monitor_Nginx
+    }
 }
 EOF
 }
@@ -74,30 +44,30 @@ function backup() {
 cat > keepalived.conf <<EOF
 ! Configuration File for keepalived
 global_defs {
-   router_id nginx
+   router_id nginx_backup
 }
 vrrp_script Monitor_Nginx {
    script "/usr/local/nginx_pid.sh"
    interval 2
-   weight 2
+   weight -10
 }
 vrrp_instance nginx {
     state BACKUP
     !state MASTER
     interface ${dev}
-    virtual_router_id 51
+    virtual_router_id 99
     priority 99
     advert_int 1
     authentication {
         auth_type PASS
-        auth_pass saintic
+        auth_pass 123456
     }
     virtual_ipaddress {
         $vip
     }
-}
-track_script {
-    Monitor_Nginx
+    track_script {
+        Monitor_Nginx
+    }
 }
 EOF
 }
@@ -117,7 +87,10 @@ chmod +x /usr/local/nginx_pid.sh
 }
 
 function keepalived() {
-  cd ${soft_dir}; tar zxf keepalived-${ha_version}.tar.gz ; cd keepalived-${ha_version}
+  #install keepalived and configure
+  cd ${soft_dir}
+  [ -d keepalived-${ha_version} ] && rm -rf keepalived-${ha_version}
+  tar zxf keepalived-${ha_version}.tar.gz ; cd keepalived-${ha_version}
   ./configure --prefix=${app_dir}/keepalived --sysconf=/etc && make && make install
   cd ${ha_conf}; mv keepalived.conf keepalived.conf.bak
   if [ "$ha_type" == "master" ];then
@@ -138,6 +111,25 @@ function iptables_rule() {
   #iptables -I INPUT -i ${dev} -s 224.0.0.18 -j ACCEPT
 }
 
+#script start
+if [ $# -ne 2 ];then
+  echo "Error:需要两个类型参数,$0 master/backup virtual_ip";
+  echo '如果需要安装Nginx服务，请执行"curl -s https://saintic.top/nginx.txt > nginx.sh && sh nginx.sh"'
+  exit 1
+fi
+#modify version and other info
+ha_version="1.2.17"
+ha_conf="/etc/keepalived"
+app_dir="/data/app"
+soft_dir="/data/software"
+ha_type=$1
+vip=$2
+dev="eth0"   #your network interface device
+#check ip format
+_check_ip=`echo $vip | egrep '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'`
+[ ! $_check_ip ] && echo "IP地址格式不正确" && exit 3
+[ -d ${soft_dir} ] || mkdir -p ${soft_dir}
+[ -d ${app_dir} ] || mkdir -p ${app_dir}
 yum -y install tar gzip wget gcc gcc-c++ make openssl-devel curl
 [ -f ${soft_dir}/keepalived-${ha_version}.tar.gz ] || download
 nginx
