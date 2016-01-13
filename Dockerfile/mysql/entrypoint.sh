e63ebeb0410a3981670b2f14fbce9b0ec7a46951
taochengwei <staugur@saintic.com>
2016-01-13 23:55:39 +0800

    mysql

--- Dockerfile/mysql/entrypoint.sh
+++ Dockerfile/mysql/entrypoint.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 /etc/init.d/mysqld start
+mysql -uroot -e "DROP DATABASE test;"
 
-if [ -z $MY_USER ]; then
-    MY_USER=root
-    MY_PASSSWD=123456
-fi
+[ -z $MY_USER ] && exit 1
+[ -z $MY_PASSWD ] && exit 1
 
-mysql -uroot -e "grant all privileges on *.* to \"${MY_USER}\"@'%' identified by \"${MY_PASSWD}\" with grant option;"
-mysql -uroot -e "grant all privileges on *.* to \"${MY_USER}\"@'localhost' identified by \"${MY_PASSWD}\" with grant option;"
+#mysql -uroot -e "grant all privileges on *.* to \"${MY_USER}\"@'%' identified by \"${MY_PASSWD}\" with grant option;"
+#mysql -uroot -e "grant all privileges on *.* to \"${MY_USER}\"@'localhost' identified by \"${MY_PASSWD}\" with grant option;"
 
-#mysql -uroot -e "grant all privileges on *.* to '"${MY_USER}"@"%"' identified by \"${MY_PASSWD}\" with grant option;"
-
-#mysql -uroot -e "grant all privileges on *.* to '"${MY_USER}"@"localhost"' identified by \"${MY_PASSWD}\" with grant option;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* to \"${MY_USER}\"@'%' IDENTIFIED BY \"${MY_PASSWD}\" WITH GRANT OPTION;"
 
 /etc/init.d/mysqld restart
 
