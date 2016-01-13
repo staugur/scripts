8d4285ca38bb852b04c8683fa7de7691d1f033db
taochengwei <staugur@saintic.com>
2016-01-13 23:21:48 +0800

    mysql

--- Dockerfile/mysql/entrypoint.sh
+++ Dockerfile/mysql/entrypoint.sh
@@ -8,6 +8,12 @@ if [ -z $MY_USER ]; then
 fi
 
 mysql -uroot -e "grant all privileges on *.* to \"${MY_USER}\"@'%' identified by \"${MY_PASSWD}\" with grant option;"
-
 mysql -uroot -e "grant all privileges on *.* to \"${MY_USER}\"@'localhost' identified by \"${MY_PASSWD}\" with grant option;"
 
+#mysql -uroot -e "grant all privileges on *.* to '"${MY_USER}"@"%"' identified by \"${MY_PASSWD}\" with grant option;"
+
+#mysql -uroot -e "grant all privileges on *.* to '"${MY_USER}"@"localhost"' identified by \"${MY_PASSWD}\" with grant option;"
+
+/etc/init.d/mysqld restart
+
+sleep 5
