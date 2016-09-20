91cddc45451d89805c9d107d137349a283c494df
Mr.tao <staugur@vip.qq.com>
2016-09-20 18:39:40 +0800

    auto commit

--- other/addkey.sh
+++ other/addkey.sh
@@ -1,10 +1,10 @@
 #/bin/bash
-user=gantian
-useradd -G wheel $user
+user=zhaoziyuan
+useradd -G docker $user
 sudo mkdir -p /home/${user}/.ssh
 sudo chmod 700 /home/${user}/.ssh
 sudo cat > /home/${user}/.ssh/authorized_keys <<'EOF'
-ssh-rsa AAAAB3NzaC1kc3MAAACBAK8QtnJm8XWCpaWLtRfjvj3Y71BGVdK7i+2TFY1faenjR/BBIYQjqB0VxzRi2neXAiNCFlqjhBst/+AgvBDFTJXGxKC1RH5V9W2r/8RJt9h0tWsEowq9KnEmW20U83BLJRcEY/Dcpu1XzxWB/WK4I0xdcrCzSZo0QBpTIr7Eh3NTAAAAFQCbkL3iePcxahfWuopktpTry+It4wAAAIBIcxlXlQHjLn4l7PVknYTiCEDs+CbS0edo99+Lp7GFULu4cd3199IK+hw2MYQld2XXyGKIHFgdMSVAlJAi6oGqz4UuV+huS43aj4lpEH/wYCLW+7sQzwosbRCDArX0zunF2S9wpkXi4eIEYKOGRbibzzVk1NktAWi14BkdQ1ZAlQAAAIB+KmaiGCzGBkUGOAaS0EaiA9oBLSizO7K+kUaG077Vm15hmscYLkE1Ld79wcdhxq079AEJKr3qrIPOzZkrsuhYbLCCL63Xs5Dl3cVHvNujYE2crxPQgskUDNz0Udzatqd4zy/a7WGZx7WNbvgPLJc4cAFBiweXEuceoSpPPX09oQ==
+ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAvBVcPNfIWM6QdQP/dxviiPUrr9Q+29alyOcySZErvBF6gesO3s23aCvU00eXfz1WmixiLeiK41BhOBd4ZO/WZjU59K+/dgbpgg0ECg+tH2e57za1AWgzWIGtI93H5EiRwCTcQnlZ0HG1TiQRUJixk40e7qRUJ3O+kG9gDkKy8ZM=
 EOF
 sudo chmod 0600 /home/${user}/.ssh/authorized_keys
 sudo chown -R ${user}:${user} /home/${user}/.ssh
