92427c887e17ffdc1fb1555f004829e0c959460b
Mr.tao <staugur@vip.qq.com>
2016-09-28 16:49:42 +0800

    auto commit

--- python/get_youtube_view.py
+++
@@ -1,9 +0,0 @@
-import time
-import webbrowser
-
-#how much views you want
-
-print("Enjoy your Time\n" +time.ctime())
-for count in range(30):
-    time.sleep(5) 
-    webbrowser.open("https://www.youtube.com/watch?v=o6A7nf3IeeA")
