7ab5a3c7727bdbda26f5e1371020a1ec0ad67116
Mr.tao <staugur@vip.qq.com>
2018-03-09 10:58:38 +0800

    win更换壁纸

--- setWallpaper/set_wallpaper.py
+++ setWallpaper/set_wallpaper.py
@@ -1,15 +1,40 @@
-# coding: utf8
-# pip install pywin32
+# -*- coding: utf-8 -*-
+"""
+    set_wallpaper
+    ~~~~~~~~~~~~~~
 
-import win32api, win32con, win32gui, os, urllib
+    Win10自动更换壁纸，壁纸来源于Bing每日一图
+
+    0. 默认的set_wallpaper.exe需要有D盘存在并有权限写入，否则请按照以下步骤重新打包
+
+    1. 下载代码并安装依赖包
+        pip install pywin32
+
+    2. 打包 
+        2.1 pip install PyInstaller
+        2.2 打开cmd(或powershell)，进入脚本所在目录，执行：
+            pyinstaller.exe -F set_wallpaper.py -i win.ico -w
+
+    3. set_wallpaper函数参数
+        img_api: 图片来源地址(默认给出的可以获取每日Bing美图)
+        img_path: 图片存放在Windows中的绝对路径
+
+    :copyright: (c) 2018 by staugur.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import os
+import urllib
+import win32api
+import win32con
+import win32gui
 
 
 def set_wallpaper(img_api="https://open.saintic.com/api/bingPic/", img_path="D:\\BingWallpaper.jpg"):
-    """ 自动更换windows壁纸，壁纸来源于Bing每日一图 """
     # 下载壁纸
     try:
         urllib.urlretrieve(img_api, filename=img_path)
-    except Exception as e:
+    except:
         raise
     else:
         # 打开指定注册表路径
@@ -20,6 +45,7 @@ def set_wallpaper(img_api="https://open.saintic.com/api/bingPic/", img_path="D:\
         win32api.RegSetValueEx(reg_key, "TileWallpaper", 0, win32con.REG_SZ, "0")
         # 刷新桌面
         win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, img_path, win32con.SPIF_SENDWININICHANGE)
+
 
 if __name__ == "__main__":
     set_wallpaper()
