53599d12c3cb6f4f8ecca5a36a0c82e53a0592d1
Mr.tao <staugur@vip.qq.com>
2018-11-29 14:53:16 +0800

    openvpn

--- services/openvpn.sh
+++ services/openvpn.sh
@@ -1,64 +1,92 @@
 #!/bin/bash
 
-[ $(uname -m) = "x86_64" ] && echo "Check OK" || exit 1
-function HEAD() {
-	if [ $(id -u) != "0" ]; then
-    	echo "Error:请确保以root用户执行此脚本！"
-    	exit 1
-	fi
-	SESTATE=$(sestatus | wc -l)
-	if [ "$SESTATE" != "1" ]; then
-		sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-		sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/selinux/config &> /dev/null
-	fi
+# Shell Env
+SHELL_DIR=$(cd $(dirname "$0") ; pwd)
+# SOFTWARE DIR
+SOFT_DIR="/usr/local/src"
+
+function precheck() {
+    [ $(uname -m) = "x86_64" ] && echo "Check OK" || exit 1
+    if [ $(id -u) != "0" ]; then
+        echo "Error:请确保以root用户执行此脚本！" ; exit 1
+    fi
+    SESTATE=$(sestatus | awk '{print $NF}')
+    if [ "${SESTATE}" != "disabled" ]; then
+        sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+        sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/selinux/config
+        echo "检测到SELinux开启中，已经关闭，需要重启系统使其生效！"
+    fi
+    ntpdate ntp.pool.org
+    systemctl stop firewalld
+    systemctl disable firewalld
 }
 
-SYS_VERSION=$(awk -F "release" '{print $2}' /etc/redhat-release | awk '{print $1}' | awk -F . '{print $1}')
+function install() {
+    cd $SOFT_DIR
+    [ "$?" != "0" ] && exit 1
 
-UpdateTime() {
-  yum -y install ntpdate
-  ntpdate pool.ntp.org &>/dev/null
+    wget -c https://static.saintic.com/download/thirdApp/OpenVPN/openvpn-2.1_rc21.tar.gz
+    [ "$?" != "0" ] && exit 1
+
+    wget -c https://static.saintic.com/download/thirdApp/OpenVPN/openvpn-2.1_rc21_eurephia.patch
+    [ "$?" != "0" ] && exit 1
+
+    yum -y install pam-devel openssl-devel lzo-devel automake gcc gcc-c++ patch pkcs11-helper pkcs11-helper-devel
+    [ "$?" != "0" ] && exit 1
+
+    tar zxf openvpn-2.1_rc19.tar.gz
+    [ "$?" != "0" ] && exit 1
+
+    cd openvpn-2.1_rc19
+    [ "$?" != "0" ] && exit 1
+
+    patch -p1 < ../openvpn-2.1_rc21_eurephia.patch
+    [ "$?" != "0" ] && exit 1
+
+    ./configure && make && make install
+    [ "$?" != "0" ] && exit 1
+
+    cd easy-rsa/2.0/ && source ./vars
+    #VPN证书方面环境变量，可自行修改
+    export KEY_COUNTRY="CN"
+    export KEY_PROVINCE="CA"
+    export KEY_CITY="Beijing"
+    export KEY_ORG="SaintIC"
+    export KEY_EMAIL="your@email.com"
+
+    ./clean-all
+
+    ./build-ca --batch
+    [ "$?" != "0" ] && exit 1
+    
+    ./build-key-server --batch server
+    [ "$?" != "0" ] && exit 1
+
+    ./build-dh
+    [ "$?" != "0" ] && exit 1
+
+    cd keys && mkdir -p /etc/openvpn
+    [ "$?" != "0" ] && exit 1
+
+    cp ca.crt ca.key dh1024.pem server.key server.crt ../../../sample-config-files/server.conf /etc/openvpn
+    [ "$?" != "0" ] && exit 1
+
+    cp ../../../sample-scripts/openvpn.init /etc/init.d/openvpn
+    [ "$?" != "0" ] && exit 1
 }
 
+function postcheck() {
+    echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.conf
+    echo "现在OpenVPN已经安装完成，您需要根据实际情况修改/etc/openvpn/server.conf配置文件。"
+    echo "修改完毕，您应该使用\"sysctl -p\"命令使转发功能生效(若已设置请忽略)。"
+    echo "现在需要添加用户，若使用证书方式，可以使用此脚本添加或吊销用户证书：https://github.com/staugur/scripts/blob/master/services/openvpn_usermanager.sh"
+    echo "启动OpenVPN命令：service openvpn start"
+}
 
-vpnsoft=/tmp/openvpn 
-vpnhome=${vpnsoft}/openvpn-2.1_rc21
-vpnconf=/etc/openvpn/
+function main() {
+    precheck
+    install
+    postcheck
+}
 
-yum -y install wget pam-devel openssl-devel lzo-devel automake gcc gcc-c++ tar gzip patch
-[ -z $vpnsoft ] && rm -rf ${vpnsoft}/* || mkdir -p $vpnsoft
-cd $vpnsoft 
-wget -c https://software.saintic.com/core/download/openvpn-2.1_rc21.tar.gz
-wget -c https://software.saintic.com/core/download/openvpn-2.1_rc21_eurephia.patch
-wget -c https://software.saintic.com/core/rpms/pkcs11-helper-1.11-4.fc22.x86_64.rpm
-wget -c https://software.saintic.com/core/rpms/pkcs11-helper-devel-1.11-4.fc22.x86_64.rpm
-rpm -ivh pkcs11-helper-*.rpm
-UpdateTime
-tar zxf openvpn-2.1_rc21.tar.gz ; cd $vpnhome
-patch -p1 < ../openvpn-2.1_rc21_eurephia.patch
-./configure --prefix=/usr/local/openvpn && make && make install
-ln -s /usr/local/openvpn/sbin/openvpn /usr/sbin/openvpn
-cd ${vpnhome}/easy-rsa/2.0/
-#if you need to modify the cert info, please change the file "vars".
-source ./vars
-./clean-all
-[ `echo $LANG` =  "zh_CN.UTF-8" ] && echo -e "\033[31m请根据提示输入证书信息:\033[0m" || echo -e "\033[31mPlease enter the certificate information:\033[0m"
-./build-ca
-./build-key-server server
-./build-dh
-[ `echo $LANG` =  "zh_CN.UTF-8" ] && echo -n -e "\033[31m请输入客户端证书名称:\033[0m" || echo -n -e "\033[31mPlease enter the name of the client certificate:\033[0m"
-read C_C
-./build-key $C_C
-[ -d $vpnconf ] || mkdir -p $vpnconf ; cd ${vpnhome}/easy-rsa/2.0/keys
-cp -fr  ca.crt  ca.key  dh1024.pem  server.key server.crt  ${C_C}.* ${vpnhome}/sample-config-files/server.conf  $vpnconf 
-cp -fr ${vpnhome}/sample-scripts/openvpn.init /etc/init.d/openvpn && chmod +x /etc/init.d/openvpn && chkconfig --add openvpn && chkconfig openvpn on
-
-/etc/init.d/openvpn start
-check_sf=$(ps aux | grep openvpn | grep -v grep | wc -l)
-if test $check_sf -ne 1 ; then
-  echo "Start Fail"
-  /usr/local/openvpn/sbin/openvpn --daemon --writepid /var/run/openvpn/server.pid --config server.conf --cd /etc/openvpn
-else
-  echo "Start Success"
-fi
-
+main
---
+++ services/openvpn_usermanager.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+#######################################################
+# $Name:         openvpn_usermanager.sh
+# $Version:      v1.0
+# $Author:       Taochengwei <taochengwei@starokay.com>
+# $Create Date:  2018-10-17
+# $Modify Date:  2018-10-30
+# $Description:  OpenVPN用户添加(用户配置生成自动化)、吊销
+#######################################################
+
+# Shell Env
+SHELL_DIR=$(cd $(dirname "$0") ; pwd)
+# EASY_RSA目录
+EASYRSA_HOME="/usr/local/src/openvpn-2.1_rc21/easy-rsa/2.0"
+# OPENVPN配置目录
+OPENVPN_CONF="/etc/openvpn"
+# OPENVPN服务器IP
+OPENVPN_SERVER_IP="47.75.99.24"
+# OPENVPN服务器监听端口
+OPENVPN_SERVER_PORT=11194
+# 服务器CA证书链
+OPENVPN_SERVER_CRT="${OPENVPN_CONF}/ca.crt"
+# 吊销的用户证书链
+OPENVPN_SERVER_CRL="${OPENVPN_CONF}/crl.pem"
+# 用户压缩包目录
+USER_PACKAGEDIR="/tmp/openvpn-user-zips"
+# 客户端软件
+CLIENT_PACKAGFILE="https://static.saintic.com/download/thirdApp/OpenVPN/openvpn-install-2.3.6-I601-x86_64.exe"
+
+#Write Log 
+function show_err(){
+    LOG_INFO=$@
+    echo -e "\033[1;31m[ERROR] ${LOG_INFO}\033[0m"
+}
+
+function show_info(){
+    LOG_INFO=$@
+    echo -e "\033[1;32m[INFO] ${LOG_INFO}\033[0m"
+}
+
+# Shell Usage
+shell_usage(){
+    echo $"Usage: $0 {build|revoke} username"; exit 1
+}
+
+# Actual function
+function build_user() {
+    username=$1
+    if [ ! -f $OPENVPN_SERVER_CRT ]; then
+        echo "The ca.crt does not exist." ; exit 1
+        return 1
+    fi
+    if [ -z $username ]; then
+        echo "The user is null." ; exit 1
+        return 1
+    fi
+    cd $EASYRSA_HOME
+    [[ -f keys/${username}.crt || -f keys/${username}.key ]] && {
+        echo "User ${username} already exist." ; exit 1
+        return 1
+    }
+
+    which unix2dos > /dev/null 2>&1
+    if [ "$?" != "0" ]; then
+        yum -y install unix2dos dos2unix
+    fi
+    which unix2dos > /dev/null 2>&1
+    [ "$?" != "0" ] && exit 1
+
+    which wget > /dev/null 2>&1
+    if [ "$?" != "0" ]; then
+        yum install -y wget
+    fi
+
+    # 制作证书
+    source ./vars > /dev/null 2>&1
+    [ "$?" != "0" ] && exit 1
+
+    export KEY_EMAIL="${username}@starokay.com"
+
+    ./pkitool $username
+    [ "$?" != "0" ] && exit 1
+
+    /bin/cp -f keys/${username}.crt keys/${username}.key ${OPENVPN_CONF}
+    [ "$?" != "0" ] && exit 1
+
+    # 生成用户配置
+    USER_TMP="$(date +%s)_tmp_${username}"
+    mkdir $USER_TMP && cd $USER_TMP
+
+    wget -c -O openvpn-install-2.3.6-I601-x86_64.exe ${CLIENT_PACKAGFILE}
+    [ "$?" != "0" ] && exit 1
+
+    # 配置文件
+    conf="client.ovpn"
+    cat > ${conf} <<EOF
+client
+dev tun
+proto udp
+remote $OPENVPN_SERVER_IP $OPENVPN_SERVER_PORT
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+#ca ca.crt
+#cert ${username}.crt
+#key ${username}.key
+remote-cert-tls server
+comp-lzo
+verb 3
+key-direction 1
+EOF
+    # 将证书私钥合并到配置文件中
+    echo "<ca>" >> ${conf}
+    cat ${OPENVPN_SERVER_CRT} >> ${conf}
+    echo "</ca>" >> ${conf}
+    echo "<cert>" >> ${conf}
+    cat ${EASYRSA_HOME}/keys/${username}.crt >> ${conf}
+    echo "</cert>" >> ${conf}
+    echo "<key>" >> ${conf}
+    cat ${EASYRSA_HOME}/keys/${username}.key >> ${conf}
+    echo "</key>" >> ${conf}
+
+    # 转换上述文件到windows格式
+    unix2dos ${conf}
+    [ "$?" != "0" ] && exit 1
+
+    # 压缩
+    zip -r ${username}.zip ./
+
+    [ -d ${USER_PACKAGEDIR} ] || mkdir -p ${USER_PACKAGEDIR}
+
+    mv ${username}.zip ${USER_PACKAGEDIR}
+    [ "$?" != "0" ] && exit 1
+
+    cd .. && rm -rf ${USER_TMP}
+
+    service openvpn restart
+    [ "$?" != "0" ] && exit 1
+
+    # 打印下载链接
+    echo -e "\033[32mBuild successfully, download with ${USER_PACKAGEDIR}/${username}.zip\033[0m"
+}
+
+function revoke_user() {
+    username=$1
+    if [ -z $username ]; then
+        echo "The user is null."
+        return 1
+    fi
+    cd $EASYRSA_HOME
+    [[ ! -f keys/${username}.crt && ! -f keys/${username}.key ]] && {
+        echo "User ${username} does not exist."
+        return 1
+    }
+    # 吊销证书
+    source ./vars > /dev/null 2>&1
+    [ "$?" != "0" ] && exit 1
+
+    export KEY_EMAIL="${username}@starokay.com"
+
+    ./revoke-full ${username}
+
+    /bin/cp -f keys/crl.pem ${OPENVPN_SERVER_CRL}
+    [ "$?" != "0" ] && exit 1
+
+    service openvpn restart
+    [ "$?" != "0" ] && exit 1
+
+    # 打印提示
+    echo -e "\033[31mRevoke successfully, you should set openvpn server.conf with \"crl-verify ${OPENVPN_SERVER_CRL}\" and restart it.\033[0m"
+    return 0
+}
+
+# Main Function
+main(){
+    # 判断脚本传参个数
+    [ "$#" -ne 2 ] && {
+        shell_usage
+    }
+    read -p "请确认是否执行\"$2 $1\", (Y/n, Default n):" en
+    if [ "$en" != "Y" ]; then
+        echo "Graceful Exit" ; exit 127
+    fi
+    case $1 in
+        build)
+            build_user $2
+        ;;
+        revoke)
+            revoke_user $2
+        ;;
+        *)
+            shell_usage
+        ;;
+    esac
+}
+
+#Exec
+main $@
