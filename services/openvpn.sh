63d09c59f58ed962a5957b95c879b134740ec573
Hiroshi.tao <staugur@saintic.com>
2020-02-25 13:26:41 +0800

    Update openvpn.sh

--- services/openvpn.sh
+++ services/openvpn.sh
@@ -76,7 +76,7 @@ function install() {
 }
 
 function postcheck() {
-    echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.conf
+    echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
     echo "现在OpenVPN已经安装完成，您需要根据实际情况修改/etc/openvpn/server.conf配置文件。"
     echo "修改完毕，您应该使用\"sysctl -p\"命令使转发功能生效(若已设置请忽略)。"
     echo "现在需要添加用户，若使用证书方式，可以使用此脚本添加或吊销用户证书：https://github.com/staugur/scripts/blob/master/services/openvpn_usermanager.sh"
